1ce5adcced92c0343ceff270d16c1549e78154c8
ElvisSantos <elvisadriansegovia@gmail.com>
2025-10-22 01:02:26 -0600

    Se agrega el Proyecto

---
+++ Ventas.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+datos = {
+    'Producto': ['Televisor', 'Celular', 'Laptop', 'Tablet', 'Audífonos'], 'Ventas': [150, 200, 250, 300, 100], 'Precio': [750, 650, 900, 400, 120] 
+    } 
+
+df = pd.DataFrame(datos) 
+
+html_tabla = df.to_html(index=False) 
+
+estadisticas = df.describe().to_html() 
+plt.bar(df['Producto'], df['Ventas']) 
+plt.title('Ventas por Producto') 
+plt.xlabel('Producto') 
+plt.ylabel('Ventas') 
+plt.xticks(rotation=30) 
+plt.tight_layout() 
+plt.savefig("grafica_ventas.png") 
+plt.close() 
+html = f""
